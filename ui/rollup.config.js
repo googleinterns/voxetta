66b678bf83ebea91cb42e82edbcd46e8fe01d5ca
Eldrick Buissereth <eldrick@google.com>
2020-06-24 23:00:35 +0000

    cleanup rollup.config.js; update prettier config

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,20 +1,15 @@
 import merge from 'deepmerge';
-// use createSpaConfig for bundling a Single Page App
-import {createSpaConfig} from '@open-wc/building-rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
 
+// use createSpaConfig for bundling a Single Page App
+import {createSpaConfig} from '@open-wc/building-rollup';
 // use createBasicConfig to do regular JS to JS bundling
 // import { createBasicConfig } from '@open-wc/building-rollup';
 
+// TODO: make SPA decision and cleanup this config further
+
 const baseConfig = createSpaConfig({
-    // use the outputdir option to modify where files are output
-    // outputDir: 'dist',
-
-    // if you need to support older browsers, such as IE11, set the legacyBuild
-    // option to generate an additional build just for this browser
-    // legacyBuild: true,
-
     // development mode creates a non-minified build for debugging or development
     developmentMode: process.env.ROLLUP_WATCH === 'true',
 
@@ -23,13 +18,7 @@ const baseConfig = createSpaConfig({
 });
 
 export default merge(baseConfig, {
-    // if you use createSpaConfig, you can use your index.html as entrypoint,
-    // any <script type="module"> inside will be bundled by rollup
     input: './index.html',
-
-    // alternatively, you can use your JS as entrypoint for rollup and
-    // optionally set a HTML template manually
-    // input: './app.js',
 
     output: {
         dir: 'dist',
