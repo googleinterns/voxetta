b09361aed280522b7d39027607d68c06d68ee80c
Ashley Granitto <granittoashley@gmail.com>
2020-06-29 22:09:31 +0000

    remove external API logic to service file

--- server/src/main/java/com/google/speech/tools/voxetta/servlets/BlobstoreLinkServlet.java
+++ server/src/main/java/com/google/speech/tools/voxetta/servlets/BlobstoreLinkServlet.java
@@ -1,21 +1,21 @@
 // Copyright 2020 Google LLC
- 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
- 
+// 
 //     https://www.apache.org/licenses/LICENSE-2.0
- 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
  
-package com.google.speech.tools.voxetta;
- 
-import com.google.appengine.api.blobstore.BlobstoreService;
-import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+package com.google.speech.tools.voxetta.servlets;
+
+import com.google.appengine.api.blobstore.BlobstoreFailureException;
+import com.google.speech.tools.voxetta.services.DatastoreUtteranceService;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,26 +23,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
  
 /** 
-* Servlet that returns a Blobstore URL. 
-*/
-@WebServlet("/blobstore-link")
+ * Servlet that returns a Blobstore URL. 
+ */
+@WebServlet("/blobstore-utterance-upload-link")
 public class BlobstoreLinkServlet extends HttpServlet {
- 
-  private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+  private DatastoreUtteranceService service = new DatastoreUtteranceService(); 
  
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Redirect to Datastore processing after Blobstore processing
-    String uploadUrl = blobstoreService.createUploadUrl("/upload-utterance");
- 
-    response.setContentType("text/html");
-    response.getWriter().println(uploadUrl);
+    response.setContentType("application/json");
+
+    // Create and return a Blobstore Upload URL  
+    try {
+      String uploadUrl = service.getFormUrl();
+      response.getWriter().println("{ \"success\": true, \"url\": \"" + uploadUrl + "\" }"); 
+    } catch (BlobstoreFailureException e) {
+      response.getWriter().println("{ \"success\": false, \"error\": \"Error: Failed to upload audio file to Blobstore.\" }");
+    }
   }
 
   /** 
-  * Allow the servlet's Blobstore service to be set for mocking purposes
-  */
-  public void setBlobstoreService(BlobstoreService inputService) {
-      blobstoreService = inputService; 
+   * Allow the servlet's Datastore Utterance Service to be set for mocking purposes.
+   */
+  public void setService(DatastoreUtteranceService inputService) {
+    service = inputService; 
   }
 }
