5230b276ee1c561a18fb8eecfe17767c8fc81fd8
Ashley Granitto <granittoashley@gmail.com>
2020-06-30 21:58:47 +0000

    add ErrorResponse object

---
+++ server/src/main/java/com/google/speech/tools/voxetta/data/ErrorResponse.java
@@ -0,0 +1,34 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.speech.tools.voxetta.data;
+
+/** 
+ * An ErrorResponse is the container for a servlet's response to a front-end fetch request upon failure.  
+ */
+public class ErrorResponse extends StatusResponse {
+ 
+  private String error; 
+
+  /**
+   * Creates a new ErrorResponse.
+   *
+   * @param success Represents if a backend fetch was successful or not. 
+   * @param error The error message associated with a given failure.
+   */ 
+  public ErrorResponse(boolean success, String error) {
+    super(success);
+    this.error = error;
+  }
+}
