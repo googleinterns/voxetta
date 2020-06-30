69cbe98d22e4af62d542670e93a4b4afcd00c936
Ashley Granitto <granittoashley@gmail.com>
2020-06-30 22:01:02 +0000

    incorporate Response objects in Utterance Upload returns

--- server/src/main/java/com/google/speech/tools/voxetta/servlets/UtteranceUploadServlet.java
+++ server/src/main/java/com/google/speech/tools/voxetta/servlets/UtteranceUploadServlet.java
@@ -18,6 +18,8 @@ import com.google.appengine.api.blobstore.BlobstoreFailureException;
 import com.google.appengine.api.datastore.DatastoreFailureException;
 import com.google.common.annotations.VisibleForTesting; 
 import com.google.speech.tools.voxetta.data.Utterance; 
+import com.google.speech.tools.voxetta.data.ErrorResponse;
+import com.google.speech.tools.voxetta.data.StatusResponse; 
 import com.google.speech.tools.voxetta.services.DatastoreUtteranceService; 
 import com.google.speech.tools.voxetta.services.UtteranceService; 
 import java.io.IOException;
@@ -33,6 +35,7 @@ import javax.servlet.http.HttpServletResponse;
 public class UtteranceUploadServlet extends HttpServlet {
 
   private UtteranceService service = new DatastoreUtteranceService(); 
+  private StatusResponse statusResponse; 
   private String audio;
 
   @Override
@@ -54,11 +57,12 @@ public class UtteranceUploadServlet extends HttpServlet {
 
     try {
       service.saveUtterance(utterance);
-      // TO DO (ASHLEY): refactor JSON in future pr
-      response.getWriter().println("{ \"success\": true }"); 
+      String successJson = StatusResponse.convertToJson(new StatusResponse(true));
+      response.getWriter().println(successJson); 
     } catch (DatastoreFailureException e) {
-      // TO DO (ASHLEY): refactor JSON in future pr
-      response.getWriter().println("{ \"success\": false, \"error\": \"Error: Failed to upload Utterance to Datastore.\" }");
+      String failureJson = ErrorResponse.convertToJson(
+          new ErrorResponse(false, "Error: Failed to upload Utterance to Datastore."));
+      response.getWriter().println(failureJson);
     }
   }
 
