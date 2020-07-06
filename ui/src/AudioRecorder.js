9d758793a0bf809f17d51fad457c94bdfce2c169
Ashley Granitto <granittoashley@gmail.com>
2020-07-06 20:38:22 +0000

    integrate front end with utterance-related back end

--- ui/src/AudioRecorder.js
+++ ui/src/AudioRecorder.js
@@ -1,16 +1,19 @@
 export class AudioRecorder {
     /**
-    * Creates an AudioRecorder that can record utterances
-    * @param {String} stores and allows access to utterance after recording
-    * @param {Object} allows access to Web Audio API
-    */
+     * Creates an AudioRecorder that can record utterances
+     * @param {String} recordingUrl - A URL that allows access to an audio file after recording.
+     * @param {Blob} blob - An audio recording file. 
+     * @param {Object} mediaRecorder - Allows access to Web Audio API.
+     */
     constructor() {
         this.recordingUrl;
+        this.blob;
         this.mediaRecorder;
     }
+
     /**
-    * Prompts user for access to Microphone using API and starts recording is access granted
-    */
+     * Prompts user for access to Microphone Component and begins recording if access is granted.
+     */
     startRecording() {
         navigator.mediaDevices.getUserMedia({ audio: true, video: false })
             .then((stream) => {
@@ -18,27 +21,28 @@ export class AudioRecorder {
                 this.mediaRecorder = mediaRecorder;
                 mediaRecorder.start();
             }).catch(function(err) {
-                console.log(err.name, err.message); // Is this good to have or is just the alert fine?
-                alert("need access to microphone to record");
+                alert("Error: Need access to microphone to record.");
             });
     }
+
     /**
-    * Stops recording and stores utterance data in Url if recording
-    * @returns {String} Url to access utterance
-    */
+     * Stops Microphone recording and obtains the relevant audio Blob and corresponding URL. 
+     * @returns {Object} Audio object containing an audio Blob and its corresponding URL.
+     */
     stopRecording() {
         if(this.mediaRecorder != null) {
             this.mediaRecorder.stop();
 
             return new Promise(resolve => {
                 this.mediaRecorder.ondataavailable = (e) => {
-                    const blob = new Blob([e.data], { type : 'audio/webm;' });
-                    this.recordingUrl = window.URL.createObjectURL(blob);
-                    resolve(this.recordingUrl);
+                    this.blob = new Blob([e.data], { type : 'audio/webm;' });
+                    this.recordingUrl = window.URL.createObjectURL(this.blob);
+                    const audio = {blob: this.blob, recordingUrl: this.recordingUrl};
+                    resolve(audio);
                 };
             });
-        }else{
-            alert("Could not record successfully");
+        } else {
+            alert("Error: Could not record successfully.");
             return null;
         }
     }   
--- ui/src/VoxettaRecordButton.js
+++ ui/src/VoxettaRecordButton.js
@@ -16,6 +16,9 @@ limitations under the License. */
 import {AudioRecorder} from './AudioRecorder';
 import {LitElement, html, css} from 'lit-element';
 
+/**
+ * Component responsible for recording and uploading audio files. 
+ */
 export class VoxettaRecordButton extends LitElement {
     static get properties() {
         return {
@@ -23,31 +26,74 @@ export class VoxettaRecordButton extends LitElement {
             audioRecorder: {type: Object},
         };
     }
+
     constructor() {
         super();
         this.isRecording = false;
         this.audioRecorder = new AudioRecorder();
     }
-    render() {  
+
+    render() {
+        this.getUploadUrl();  
         return html`
             <button @click=${this.recordHandler}>Record Voice</button>
             <audio id="utterance" controls src="" style="display: none"></audio>
         `;
     }
 
-     async recordHandler(e){
+    /**
+     * If the user is not currently recording, begin recording using the Microphone component. Otherwise,
+     * stop recording and save and display the just-recorded audio file.
+     */
+    async recordHandler(){
         if (!this.isRecording) {
             this.isRecording = true;
             this.audioRecorder.startRecording();
         } else {
-            const audioSave = this.shadowRoot.getElementById("utterance");
             this.isRecording = false;
-            const recordingUrl = await this.audioRecorder.stopRecording();
-            if(recordingUrl != null){
-                audioSave.src = recordingUrl;
+            const audioSave = this.shadowRoot.getElementById("utterance");
+            const audio = await this.audioRecorder.stopRecording();
+            
+            // If non-empty, save and display the just-recorded audio file
+            if(audio.recordingUrl != null){
+                audioSave.src = audio.recordingUrl;
                 audioSave.style.display = "block";
+                this.saveAudio(audio);
             }
         }
+    }
+
+    /**
+     * Set the blobUrl property to be a Blobstore upload link. 
+     */
+    async getUploadUrl() {
+        const response = await fetch('/blobstore-utterance-upload-link');
+        const query = await response.json();
+
+        if (query.success) {
+            this.blobUrl = query.url; 
+        } else {
+            alert("Error: Unable to access database.");
+        }
+    } 
+
+    /**
+     * Save a recorded audio file to an external database. 
+     * @param {Object} audio - An object containing an audio Blob and its corresponding URL.
+     */
+    async saveAudio(audio) {
+        const formData = new FormData();
+        formData.append('audio', audio.blob, 'blob');
+
+        const response = await fetch(this.blobUrl, { 
+            method: 'POST',
+            body: formData 
+        });
+        const query = await response.json; 
+
+        if (!query.success) {
+            alert("Error: Unable to upload file.");
+        } 
     }
 }
 
