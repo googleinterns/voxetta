5a15987d9d459baa830fdd7e42a4542e1da743cd
Eldrick Buissereth <eldrick@google.com>
2020-07-10 20:52:26 +0000

    minor fix to pass tests

--- server/src/main/java/com/google/speech/tools/voxetta/services/DatastorePromptService.java
+++ server/src/main/java/com/google/speech/tools/voxetta/services/DatastorePromptService.java
@@ -88,7 +88,7 @@ public class DatastorePromptService implements PromptService {
 
         // if none, return empty json
         if (unreadQueries.size() < 1) {
-            return gson.toJson(unreadQueries);
+            return gson.toJson(new Object());
         }
 
         // TODO(eldrickb): transactionize
